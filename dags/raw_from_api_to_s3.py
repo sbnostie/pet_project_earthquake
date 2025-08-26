dfc3c40b17ccc309e3b6d9a7e3a1259171b6ca87
sb <sbnostieeee@gmail.com>
2025-08-26 15:17:24 +0500

    DAG успешно настроен

--- dags/raw_from_api_to_s3.py
+++ dags/raw_from_api_to_s3.py
@@ -60,11 +60,9 @@ def get_and_transfer_api_data_to_s3(**context):
 
 with DAG(
     dag_id=DAG_ID,
-    schedule_interval="0 5 * * *",
     default_args=args,
     tags=["s3", "raw"],
     description="SHORT_DESCRIPTION",
-    concurrency=1,
     max_active_tasks=1,
     max_active_runs=1,
 ) as dag:
